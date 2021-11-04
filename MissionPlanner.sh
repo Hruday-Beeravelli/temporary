060aa4ef0f630d9916077fc827c013361763aa5f
Hruday-Beeravelli <88395871+Hruday-Beeravelli@users.noreply.github.com>
2021-11-04 14:32:29 +0530

    Add files via upload

---
+++ MissionPlanner.sh
@@ -0,0 +1,13 @@
+sudo apt install apt-transport-https dirmngr gnupg ca-certificates -y
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+
+echo "deb https://download.mono-project.com/repo/debian stable-raspbianbuster main" | sudo tee /etc/apt/sources.list.d/monoofficial-stable.list
+
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install mono-complete -y
+
+sudo apt install festival -y
